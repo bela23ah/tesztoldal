95801b7d410472ebd0403049110b0d018a1aa25c
bela23ah <gyorgy.harkai@gmail.com>
2026-09-25 14:05:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // =========================================================================
-// Lutra Album Cserebere - Service Worker (sw.js v3.996)
+// Lutra Album Cserebere - Service Worker (sw.js v3.997)
 // =========================================================================
 
 const CACHE_NAME = 'lutra-csere-v2.0';
